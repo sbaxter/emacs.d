ca4c0bc3e2088ea6c43e29ede0cd0fb382d040cf
Seth Baxter <sbaxter@roomkey.com>
2015-02-11 10:02:52 -0500

    Remove theme autoload

--- themes/homebrew-theme.el
+++ themes/homebrew-theme.el
@@ -129,9 +129,4 @@
 
     )))
 
-;;;###autoload
-(when (and (boundp 'custom-theme-load-path) load-file-name)
-  (add-to-list 'custom-theme-load-path
-               (file-name-as-directory (file-name-directory load-file-name))))
-
 (provide-theme 'homebrew)
